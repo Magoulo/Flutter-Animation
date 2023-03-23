5803cbf8c68502ffc3fd9a89e5d675cb77d83446
Sabina <90258714+SeitansPilgrim@users.noreply.github.com>
2023-03-23 17:46:41 +0100

    Implemented the API animation

--- flutter_animation/lib/screens/apiAnimation.dart
+++ flutter_animation/lib/screens/apiAnimation.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class ApiAnimationScreen extends StatelessWidget {
+class ApiAnimationScreen extends StatefulWidget {
   const ApiAnimationScreen({super.key});
 
   @override
+  State<ApiAnimationScreen> createState() => _ApiAnimationScreenState();
+}
+
+class _ApiAnimationScreenState extends State<ApiAnimationScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('API Animation'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Play'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: const <Widget>[
+                Text(
+                  'API Animation',
+                ),
+              ],
+            ),
+            Container(
+              child: Lottie.asset(
+                'assets/animations/72-favourite-app-icon.json',
+                controller: _controller,
+                repeat: true,
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  _controller.forward();
+                },
+                child: const Text('Play'),
+              ),
+            ),
+          ],
         ),
       ),
     );
