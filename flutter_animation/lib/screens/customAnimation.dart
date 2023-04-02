af4a6acf630f99fe938e3aaa5f70b46592ef3f31
Sabina <90258714+SeitansPilgrim@users.noreply.github.com>
2023-04-02 13:12:43 +0200

    Tween box animation

--- flutter_animation/lib/screens/customAnimation.dart
+++ flutter_animation/lib/screens/customAnimation.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -11,6 +13,10 @@ class CustomAnimationScreen extends StatefulWidget {
 class _CustomAnimationScreenState extends State<CustomAnimationScreen>
     with TickerProviderStateMixin {
   late final AnimationController _controller;
+  double value = 150;
+  double _x = 0;
+  double _y = 0;
+  Color _color = Colors.blue;
 
   @override
   void initState() {
@@ -30,31 +36,65 @@ class _CustomAnimationScreenState extends State<CustomAnimationScreen>
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: const <Widget>[
-                Text(
-                  'Custom Animation',
+        body: Stack(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /*const Align(
+                alignment: FractionalOffset.topCenter,
+                child: Text(
+                  'Api Animation',
                 ),
-              ],
-            ),
-            TweenAnimationBuilder(
-                tween: star, duration: duration, builder: builder),
-            TweenAnimationBuilder(
-                tween: dots, duration: duration, builder: builder),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  _controller.forward();
+              ),*/
+              TweenAnimationBuilder(
+                tween:
+                    Tween<Offset>(begin: Offset(150, 0), end: Offset(150, _y)),
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                builder: (context, Offset offset, child) {
+                  return Positioned(
+                    left: offset.dx,
+                    top: offset.dy,
+                    child: TweenAnimationBuilder(
+                      tween: Tween<Color>(begin: Colors.blue, end: _color),
+                      duration: const Duration(seconds: 0),
+                      builder: (context, Color color, child) {
+                        return Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: color,
+                              borderRadius: BorderRadius.circular(8)),
+                        );
+                      },
+                      child: child,
+                    ),
+                  );
                 },
-                child: const Text('Play'),
+                child: Container(),
               ),
-            ),
-          ],
-        ),
+              Positioned(
+                left: MediaQuery.of(context).size.width /
+                        2 - /*(object width = 100)*/
+                    100 / 2,
+                bottom: 0 + 50,
+                child: SizedBox(
+                  width: 100,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        // _x = _x == 0 ? 100 : 0;
+                        _y = _y == 0 ? 400 : 0;
+                        _color =
+                            _color == Colors.blue ? Colors.red : Colors.blue;
+                      });
+                    },
+                    child: const Text('Play'),
+                  ),
+                ),
+              ),
+            ]),
       ),
     );
   }
